37d8c69e674c230af9342623bd02b98f1884fa1b
Kyle Carberry <kyle@carberry.com>
2024-10-18 19:37:22 -0400

    Add wasm

--- site/build_wasm.sh
+++ site/build_wasm.sh
@@ -7,6 +7,6 @@ cd "$(dirname "$0")"
 echo "WARNING: make sure you're using 'nix develop' for the correct go version"
 
 GOOS=js GOARCH=wasm go build -ldflags="-s -w" -o ./app/assets/main.wasm ../cmd/wasm
-# wasm-opt -Oz ./app/assets/main.wasm -o ./app/assets/main.wasm --enable-bulk-memory
+wasm-opt -Oz ./app/assets/main.wasm -o ./app/assets/main.wasm --enable-bulk-memory
 
-# cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" ./app/assets/wasm_exec.js
+cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" ./app/assets/wasm_exec.js
--- site/context/wush.ts
+++ site/context/wush.ts
@@ -1,3 +1,79 @@
-import React from "react";
+import React, { useEffect } from "react";
+import wasmUrl from "../assets/main.wasm";
+import "../assets/wasm_exec"
 
 export const WushContext = React.createContext<Wush | null>(null);
+
+let wasmModule: Promise<WebAssembly.WebAssemblyInstantiatedSource>
+
+export const createWush = async (): Promise<Wush> => {
+  const go = new Go();
+
+  if (!wasmModule) {
+    wasmModule = WebAssembly.instantiateStreaming(
+        fetch(wasmUrl),
+        go.importObject
+      );
+  }
+  const module = await wasmModule
+
+  return new Promise<Wush>((resolve, reject) => {
+// Start the WASM module
+go.run(module.instance).then(() => {
+    reject("Exited immediately")
+})
+  })
+    
+
+  
+};
+
+useEffect(() => {
+  // Check if not running on the client-side
+  if (typeof window === "undefined") {
+    return;
+  }
+
+  const url =
+    process.env.NODE_ENV === "development"
+      ? wasmUrl
+      : `https://storage.googleapis.com/wush-assets-prod${wasmUrl}.gz`;
+
+  console.log("loading wasm");
+  async function loadWasm(go: Go) {
+    console.log("actually load wasm");
+    const wasmModule = await WebAssembly.instantiateStreaming(
+      fetch(url),
+      go.importObject
+    );
+
+    go.run(wasmModule.instance).then(() => {
+      console.log("wasm exited");
+      setWushCtx(null);
+    });
+
+    newWush({
+      onNewPeer: (peer: Peer) => void {},
+      onIncomingFile: (peer, filename, sizeBytes): boolean => {
+        return false;
+      },
+      downloadFile: async (
+        peer,
+        filename,
+        sizeBytes,
+        stream
+      ): Promise<void> => {},
+    }).then((wush) => {
+      console.log(wush.auth_info());
+      setWushCtx(wush);
+    });
+  }
+  loadWasm(go);
+  return () => {
+    console.log("Disposing wasm");
+    if (!go.exited) {
+      exitWush();
+    }
+    setWushCtx(null);
+  };
+}, []);
