1ff2c900451e11ae43666d36a0651f8a9142e082
Colin Adler <colin1adler@gmail.com>
2024-12-13 11:54:28 -0600

    chore: update copy when printing auth key

--- cmd/wush/serve.go
+++ cmd/wush/serve.go
@@ -84,10 +84,10 @@ func serveCmd() *serpent.Command {
 			// Ensure we always print the auth key on stdout
 			if isatty.IsTerminal(os.Stdout.Fd()) {
 				hlog("Your auth key is:")
-				fmt.Println("\t>", cliui.Code(r.ClientAuth().AuthKey()))
+				fmt.Println("  >", cliui.Code(r.ClientAuth().AuthKey()))
 				hlog("Use this key to authenticate other " + cliui.Code("wush") + " commands to this instance.")
 				hlog("Visit the following link to connect via the browser:")
-				fmt.Println("\t>", cliui.Code("https://wush.dev#"+r.ClientAuth().AuthKey()))
+				fmt.Println("  >", cliui.Code("https://wush.dev#"+r.ClientAuth().AuthKey()))
 			} else {
 				fmt.Println(cliui.Code(r.ClientAuth().AuthKey()))
 				hlog("The auth key has been printed to stdout")
@@ -105,7 +105,10 @@ func serveCmd() *serpent.Command {
 				return err
 			}
 
-			ts.Up(ctx)
+			_, err = ts.Up(ctx)
+			if err != nil {
+				return fmt.Errorf("bring wireguard up: %w", err)
+			}
 			fs := afero.NewOsFs()
 
 			// hlog("WireGuard is ready")
