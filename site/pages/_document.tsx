705edff4edc53d533b283c9daeb3b47fa4198a16
Colin Adler <colin1adler@gmail.com>
2024-12-16 12:43:34 -0600

    site: add GA

--- site/pages/_document.tsx
+++ site/pages/_document.tsx
@@ -14,6 +14,22 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
           />
           <link rel="manifest" href="/site.webmanifest" />
+
+          {/* Google Analytics */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-076XNSZ8PF"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-076XNSZ8PF');
+              `,
+            }}
+          />
         </Head>
         <body className="bg-bg text-fg">
           <Main />
